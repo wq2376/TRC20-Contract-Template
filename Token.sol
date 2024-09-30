82c8b6826a3f55b4feb38dd6b1673ca1fe75372e
wq2376 <lywgs8567@163.com>
2024-09-30 20:38:32 +0800

    Update Token.sol

--- Token.sol
+++ Token.sol
@@ -16,7 +16,7 @@ contract Token is TRC20, TRC20Detailed {
     /**
      * @dev Constructor that gives msg.sender all of existing tokens.
      */
-    constructor () public TRC20Detailed("GXSDDB", "GSYYB", 18) {
+    constructor () public TRC20Detailed("GXSYYDDB", "GSYYB", 18) {
         _mint(msg.sender, 1000000000 * (10 ** uint256(decimals())));
     }
 }
